a927da9616022ec748e9f2be78b9eeb4d001ba46
Aleksandar Kis <aleksandaraaco@gmail.com>
2025-10-04 21:01:35 +0200

    Dodao sam py fajl tamo gde treba da bude

---
+++ TicTacToe.py
@@ -0,0 +1,8 @@
+# Description of the project:Idea is to create TicTacToe game. Create "numpad" logic as on keyboard. Two players should play, first player will choose
+# will be 'X' or 'O', when first player decide on screen "First player will start the game" should pop up, then message "Are you ready:", user can write Y or N
+# If Y, user will be asked to write on which position wants to X(or O) also table for tic-tac shoul be shown..
+
+# I will start my code with function for table screening
+
+def display_board(board):
+    pass
